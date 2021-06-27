2fb66919354efd048e93fe3247b8c6ff7ce3d366
Jeongsoowon <waterpurifier@khu.ac.kr>
2021-06-27 15:56:30 +0900

    Update 2021.6.27

--- getData.py
+++ getData.py
@@ -7,6 +7,9 @@
     ### v1 : 모든 기능을 통합! 
 """
 from selenium import webdriver
+import pandas as pd
+from collections import deque
+
 class getData:
     # task_dict : 업무 종류, area_dict : 지역, industryCd_dict : 공사 종류
     def __init__(self):
@@ -63,6 +66,7 @@ class getData:
             result = [results[i * 12:(i + 1) * 12] for i in range((len(results) + 12 - 1)// 12) if myArea in results[i * 12:(i + 1) * 12] ]
         return result
 
+    # 현재 페이지에서 입찰목록을 크롤링하여 결과를 반환함.
     def searchWork(self, myArea, html):
         try:
             # 크롬 브라우저를 띄우기 위해, 웹드라이버를 가져온다.
@@ -87,3 +91,104 @@ class getData:
         finally:
             driver.quit()
             return result
+
+    # 크롤링 봇으로 해당하는 날짜의 모든 입찰목록을 반환함.
+    def workListBot(self, task, area, industry, myArea, fromDay, toDay, step):
+        result = []
+        From = fromDay
+        To = [fromDay[0], fromDay[1] + step, fromDay[2]]
+        # 연도가 바뀌는 경우 설정
+        if To[1] > 12:
+            To[0] += 1
+            To[1] -= 12
+        To = [int(To[0]), int(To[1]), int(To[2])]
+        searchContinue = (To[0] == toDay[0] and To[1] == toDay[1]) or (To[0] == toDay[0] and To[1] > toDay[1])
+        html = self.makeHTML(task, area, industry, From, To)
+        result.extend(self.searchWork(myArea, html))
+        while not searchContinue:
+            To = [int(To[0]), int(To[1]), int(To[2])]
+            From = To
+            To = [From[0], From[1] + step, From[2]]
+            searchContinue = (To[0] == toDay[0] and To[1] == toDay[1]) or (To[0] == toDay[0] and To[1] > toDay[1])
+            if To[1] > 12:
+                To[0] += 1 
+                To[1] -= 12
+            html = self.makeHTML(task, area, industry, From, To)
+            result.extend(self.searchWork(myArea, html))
+            
+            if searchContinue : break
+            
+        # 업무, 공고번호-차수, 분류, 공고명, 공고기관, 수요기관, 계약방법, 입력일시(입찰마감일시), 바로가기
+        work = pd.DataFrame(columns=['업무', '공고번호', '분류', '공고명', '공고기관', '수요기관', '계약방법', '입력일시', '투찰' ,'바로가기'])
+    
+        for idx, w in enumerate(result):
+            t = w[:2] + w[3:5] + w[6:10] + w[11:12]
+            t.append(w[2])
+            work.loc[idx] = t
+            
+
+        return work, result
+
+    # 개찰결과를 반환.
+    def bidListBot(self, driver, workList):
+        bid = []
+        for w in workList:
+            try:
+                # workList의 url 열기
+                driver.get(w[2])
+                # 재입찰인지 판단 후 개찰결과 페이지로 이동
+                span_list = driver.find_elements_by_tag_name('span')
+                re = 0
+                for s in span_list:
+                    if s.text == '재입찰':
+                        re += 1
+                script = "javascript:toDetail('3', '" + w[1][:11] +"', '00', '0', '"+ str(re)  +"', '개찰완료');"
+                driver.execute_script(script)
+
+                # 개찰결과 가져와 저장 
+                elem = driver.find_element_by_class_name('results')
+                div_list = elem.find_elements_by_tag_name('div')
+                bidResult = []
+                for div in div_list:
+                    bidResult.append(div.text)
+                
+                bid.append([bidResult[i * 9:(i + 1) * 9] for i in range((len(bidResult) + 9 - 1)// 9)])
+
+            except Exception as e:
+                print(e)
+                
+        driver.quit()
+        return bid
+    
+"""
+check, count = False, 0
+    workList = []
+    while True:
+        if count == 0:
+            tempStart = start
+            # add 개월 간격으로 검색.
+            tempEnd = [start[0], start[1] + add, start[2]]
+            if tempEnd[1] > 12:
+                tempEnd[0] += 1
+                tempEnd[1] -= 12
+            count += 1
+        else:
+            tempStart = [int(tempEnd[0]), int(tempEnd[1]), int(tempEnd[2])]
+            print(tempEnd)
+            tempEnd = [tempStart[0], tempStart[1] + add, tempStart[2]]
+            if tempEnd[1] > 12:
+                tempEnd[0] += 1
+                tempEnd[1] -= 12
+            if tempEnd[0] == end[0] and tempEnd[1] == end[1]:
+                check = True
+            elif tempEnd[0] == end[0] and tempEnd[1] > end[1]:
+                tempEnd[1] = end[1]
+                check = True
+       
+        workList.extend(searchTask('공사', '강원', kind, '강원도 횡성군', tempStart, tempEnd))
+
+        if check: # 수정해야함.!
+            break
+        
+
+"""
